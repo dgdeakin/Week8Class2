8abdff8ef7e6755609890f7e8407bf463bf58eda
Dev Gurung <d.gurng@deakin.edu.au>
2023-09-05 14:48:24 +1000

    first commit

---
+++ weekclass2/src/components/icons/MyModel.vue
@@ -0,0 +1,42 @@
+<script setup>
+// export default {
+//   data() {
+//     return {
+//       open: false
+//     }
+//   }
+// }
+
+import { ref } from "vue";
+
+const open = ref(false);
+</script>
+
+<template>
+  <div>
+    <button @click="open = true">Open Modal</button>
+
+    <!-- <div v-if="open" class="modal">
+      <p>Hello from the modal!</p>
+      <button @click="open = false">Close</button>
+    </div> -->
+    <Teleport to="body">
+      <div v-if="open" class="modal">
+        <p>Hello from the modal!</p>
+        <button @click="open = false">Close</button>
+      </div>
+    </Teleport>
+  </div>
+</template>
+
+<style scoped>
+.modal {
+  position: fixed;
+  z-index: 999;
+  top: 20%;
+  left: 50%;
+  width: 300px;
+  margin-left: -150px;
+  background-color: bisque;
+}
+</style>
---
+++ weekclass2/src/views/AboutView.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { ref } from "vue";
+import { useMouse } from "../reusables/mouse.js";
+
+const { x, y } = useMouse();
+
+const vFocus = {
+  mounted: (el) => el.focus(),
+};
+
+import MyModal from "../components/icons/MyModel.vue";
+
+const show = ref(true);
+</script>
+
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <h1>Mouse Tracker</h1>
+    The mouse position is at :
+    <h1>{{ x }}, {{ y }}</h1>
+
+    <h1>Custom Directive Example</h1>
+
+    <input type="text" v-focus />
+
+    <div>
+      <h1>Transition</h1>
+
+      <button @click="show = !show">Toggle</button>
+      <Transition>
+        <p v-if="show">hello</p>
+      </Transition>
+    </div>
+
+    <div>
+      <h1>Teleport Concept</h1>
+
+      <div>
+        <MyModal />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
